291321f920e52e1b7626e19561b1b276a9b5665b
Sourav Kumar <97384250+sourav-kumar4297@users.noreply.github.com>
2022-12-15 20:51:05 +0530

    Update hypt.js

--- hypotenuse/hypt.js
+++ hypotenuse/hypt.js
@@ -6,11 +6,19 @@ function calculateSumOfSquares(a,b) {
     const sumOfSquares = a*a + b*b;
     return sumOfSquares;
 }
-function calculateHypotenuse(){
-  const sumOfSquares = calculateSumOfSquares(Number(sides[0].value),Number(sides[1].value));
-  const lengthOfHypotenuse = Math.sqrt(sumOfSquares);
-  outputEl.innerText = "The length of Hypotenuse is = " + lengthOfHypotenuse.toFixed(2);
 
+function calculateHypotenuse(){
+    if (sides[0].value === "" || sides[1].value ==="") {
+      outputEl.innerText = "Enter value for both sides";
+    } 
+    else if (sides[0].value < 0 || sides[1].value < 0) {
+      outputEl.innerText = "Side can not be negative";
+    } 
+    else {
+      const sumOfSquares = calculateSumOfSquares(Number(sides[0].value),Number(sides[1].value));
+      const lengthOfHypotenuse = Math.sqrt(sumOfSquares);
+      outputEl.innerText = "The length of Hypotenuse is = " + lengthOfHypotenuse.toFixed(2);
+    }
 }
 
 hypotenuseBtn.addEventListener('click',calculateHypotenuse);
